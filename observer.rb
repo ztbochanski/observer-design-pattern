41e6d2946eec259076f2897b414998c11f320b23
zach <ztbochanski@gmail.com>
2022-02-01 11:43:38 -0800

    add observer that includes a observable and observer

---
+++ observer.rb
@@ -0,0 +1,40 @@
+# Zachary Bochanski
+# Observer Pattern
+
+class WeatherStation
+
+  attr_accessor :temperature
+
+  def initialize
+    @temperature = 0
+    @observers = []
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def temperature=(new_temperature)
+    @temperature = new_temperature
+    @observers.each do |o|
+      o.notify(self)
+    end
+  end
+
+end
+
+class StationObserver
+
+  def notify(station)
+    puts "station notified", station.temperature
+  end
+
+end
+
+weather_station = WeatherStation.new
+
+station_observer = StationObserver.new()
+weather_station.add_observer(station_observer)
+
+
+weather_station.temperature = 72
